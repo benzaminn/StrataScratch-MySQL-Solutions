2c258e6ce4b0012ef03aa82f251eff8603039efe
benzaminn <60427530+benzaminn@users.noreply.github.com>
2023-08-02 11:22:16 +0530

    Create New Products.sql

---
+++ New Products.sql
@@ -0,0 +1,5 @@
+
+
+select company_name, count(case when (year = 2020) then 1  end) - count(case when (year = 2019) then 1 end ) as total_launch from car_launches
+group by company_name
+order by 1;
